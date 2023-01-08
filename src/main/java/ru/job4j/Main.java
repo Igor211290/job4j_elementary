5b5d583993412c276970a4fec573bd5f14d533d8
Соня <igor-rukhlov90@mail.ru>
2023-01-08 23:39:15 +0300

    Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,14 +1,8 @@
 package ru.job4j;
 
+
 public class Main {
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    ;
-
-    public static <String> void main(String[] args) {
-        System.out.println("heloworld");
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
     }
 }
